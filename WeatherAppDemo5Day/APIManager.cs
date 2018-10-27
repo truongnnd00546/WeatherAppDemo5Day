3a207c2f7ace41dae186c73c205d6dba48b1ba52
truongnnd00546 <truongnnd00546@fpt.edu.vn>
2018-10-27 08:49:19 +0700

    Ok

---
+++ WeatherAppDemo5Day/APIManager.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherAppDemo5Day
+{
+    public class APIManager
+    {
+        public async static Task<RootObject> GetWearther(double lat, double lon)
+        {
+            var http = new HttpClient();
+
+            var url = String.Format("http://api.openweathermap.org/data/2.5/forecast?lat={0}&lon={1}&appid=c68e0c1dac60c8bbd012c2ea10944d4c&units=metric&fbclid=IwAR0g48LyGf2tskjU5enEx9n9kbFMGt5dWYoZnzWz8ZzG9li38mB3ThOn-3A", lat, lon);
+            var response = await http.GetAsync(url);
+            var result = await response.Content.ReadAsStringAsync();
+            var serializer = new DataContractJsonSerializer(typeof(RootObject));
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(result));
+            var data = (RootObject)serializer.ReadObject(ms);
+            return data;
+
+
+        }
+
+
+        public class Main
+        {
+            public double temp { get; set; }
+            public double temp_min { get; set; }
+            public double temp_max { get; set; }
+            public double pressure { get; set; }
+            public double sea_level { get; set; }
+            public double grnd_level { get; set; }
+            public int humidity { get; set; }
+            public double temp_kf { get; set; }
+        }
+
+        public class Weather
+        {
+            public int id { get; set; }
+            public string main { get; set; }
+            public string description { get; set; }
+            public string icon { get; set; }
+        }
+
+        public class Clouds
+        {
+            public int all { get; set; }
+        }
+
+        public class Wind
+        {
+            public double speed { get; set; }
+            public double deg { get; set; }
+        }
+
+        public class Rain
+        {
+            public double? __invalid_name__3h { get; set; }
+        }
+
+        public class Sys
+        {
+            public string pod { get; set; }
+        }
+
+        public class List
+        {
+            public int dt { get; set; }
+            public Main main { get; set; }
+            public List<Weather> weather { get; set; }
+            public Clouds clouds { get; set; }
+            public Wind wind { get; set; }
+            // public Rain rain { get; set; }
+            public Sys sys { get; set; }
+            public string dt_txt { get; set; }
+        }
+
+        public class Coord
+        {
+            public double lat { get; set; }
+            public double lon { get; set; }
+        }
+
+        public class City
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+            public Coord coord { get; set; }
+            public string country { get; set; }
+            public int population { get; set; }
+        }
+
+        public class RootObject
+        {
+            public string cod { get; set; }
+            public double message { get; set; }
+            public int cnt { get; set; }
+            public List<List> list { get; set; }
+            public City city { get; set; }
+        }
+    }
+}
---
+++ WeatherAppDemo5Day/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using static WeatherAppDemo5Day.APIManager;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace WeatherAppDemo5Day
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            collection = new ObservableCollection<List>();
+            this.DataContext = this;
+        }
+        public ObservableCollection<List> collection { get; set; }
+
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            var postion = await LocationData.getPosition();
+            var lat = postion.Coordinate.Latitude;
+            var lon = postion.Coordinate.Longitude;
+
+            RootObject forecast = await APIManager.GetWearther(lat, lon);
+            for (int i = 0; i < 5; i++)
+            {
+                collection.Add(forecast.list[i]);
+
+            }
+            ForeCast.ItemsSource = collection;
+
+
+        }
+    }
+}
